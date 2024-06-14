84fb143e7df528087ba4b251cb2a2b299706ecf4
DanX3N0 <danieldv283@gmail.com>
2024-06-14 11:41:12 -0400

    add template

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define FUN ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define all(x) (x).begin(), (x).end()
+#define clr(a,h) memset(a,(h),sizeof(a))
+#define f first
+#define s second
+#define fore(i, b, e) for (int i = (int) b, o_o = e; i < (int) o_o; ++i)
+#define forr(i, b, e) for (int i = (int) b, o_o = e; i < (int) o_o; ++i)
+#define sz(x) (int) x.size()
+#define endl '\n'
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+
+const int INF = numeric_limits<int>::max()/4;
+const double PI = acos(-1);
+
+signed main(){FUN;
+
+  return 0;
+}
